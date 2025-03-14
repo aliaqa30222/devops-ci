d4565f215431b94c227899e4acacc7f9dedc3491
aliaqa3022 <aliaqa3022@gmail.com>
2025-03-14 01:00:44 +0000

    Fixed non-integer input handling

--- Dec2Hex.py
+++ Dec2Hex.py
@@ -1,6 +1,9 @@
 import sys
 
 def decimal_to_hex(decimal_value):
+    if not isinstance(decimal_value, int):
+        raise ValueError("Input must be an integer.")
+
     hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     hexadecimal = ""
     num = decimal_value
